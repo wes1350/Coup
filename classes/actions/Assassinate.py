18ad278786f4e92322b0d543c4be40415fca8bd6
Wesley Runnels <wrunnels@mit.edu>
2020-04-05 19:33:35 -0400

    Establish game loop while querying and processing input with bare minimum functionality.

--- classes/actions/Assassinate.py
+++ classes/actions/Assassinate.py
@@ -1,5 +1,5 @@
 from .Action import Action
 
 class Assassinate(Action):
-    def __init__(self, target_id, card_id):
-        super.__init__("kill"=True, "target"=target_id, "kill_card_id"=card_id, "cost"=3)
+    def __init__(self, target, card_id):
+        super().__init__(kill=True, target=target, kill_card_id=card_id, cost=3)
