ee344e1191726c2ccea185aabf41258c6b3415df
Wesley Runnels <wrunnels@mit.edu>
2020-04-05 23:54:49 -0400

    Remove target -1 attribute from Income, ForeignAid, Tax due to player -1 bug and general proneness to bugs

--- classes/actions/ForeignAid.py
+++ classes/actions/ForeignAid.py
@@ -2,4 +2,4 @@ from .Action import Action
 
 class ForeignAid(Action):
     def __init__(self):
-        super().__init__(cost=-2, target=-1)
+        super().__init__(cost=-2)
--- classes/actions/Steal.py
+++ classes/actions/Steal.py
@@ -2,6 +2,6 @@ from .Action import Action
 
 class Steal(Action):
     def __init__(self, target : int, n_coins : int = 2):
-        if target == -1:
-            raise ValueError("Cannot steal from the bank")
+        if target is None:
+            raise ValueError("Must specify target to steal from")
         super().__init__(steal=True, cost=-1*n_coins, target=target)
