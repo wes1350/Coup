e72e69680dedeef34df6abb9ce4e9a87fe8379da
Wesley Runnels <wrunnels@mit.edu>
2020-04-06 13:39:33 -0400

    Update Block and Challenge

--- classes/reactions/Challenge.py
+++ classes/reactions/Challenge.py
@@ -1,5 +1,5 @@
 from . import Reaction
 
 class Challenge(Reaction):
-    def __init__(self, source : int, target : int):
-        super().__init__(reaction_type="challenge", from_player=source, to_player=target)
+    def __init__(self, source : int):
+        super().__init__(reaction_type="challenge", from_player=source)
