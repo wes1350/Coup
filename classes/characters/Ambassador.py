065ded19f968677f40bc2fb1c2b4761e25364d7b
Wesley Runnels <wrunnels@mit.edu>
2020-04-09 21:44:44 -0400

    Add descriptions for all characters

--- classes/characters/Ambassador.py
+++ classes/characters/Ambassador.py
@@ -5,5 +5,8 @@ class Ambassador(Character):
     def __init__(self):
         pass        
     
+    def description(self) -> str:
+        return "Ambassadors can do the following:\n    Exchange: Exchange cards with the Court. First take 2 random cards from the Court deck. Choose which, if any, to exchange with your face-down cards. Then return two cards to the Court deck. \n    Block: Steal"
+
     def __str__(self):
         return "Ambassador"
--- classes/characters/Contessa.py
+++ classes/characters/Contessa.py
@@ -5,5 +5,8 @@ class Contessa(Character):
     def __init__(self):
         pass
 
+    def desciption(self):
+        return "Contessas can do the following:\n    Block: Assassination"
+
     def __str__(self):
         return "Contessa"
