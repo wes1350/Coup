325ec36f454c1bec17481710136704847ade3c80
Wesley Runnels <wrunnels@mit.edu>
2020-08-05 08:29:48 -0400

    Average over new elos during elo update if an agent is present more than once in agent list

--- coup/utils/elo_utils.py
+++ coup/utils/elo_utils.py
@@ -14,6 +14,7 @@ def get_agent_elos(agents):
     return [get_agent_elo(agent) for agent in agents]
 
 def update_agent_elo(agent, elo, n_games, commit=True):
+    # agent can be an Agent or its string representation
     db_entry = AgentType.query.filter(AgentType.name == str(agent)).first()
     db_entry.set_elo(elo)
     db_entry.add_games(n_games)
@@ -21,12 +22,33 @@ def update_agent_elo(agent, elo, n_games, commit=True):
     if commit:
         db.session.commit()
 
-def update_agent_elos(agents, elos, n_games_per_agent):
+def update_agent_elos(agents, elos, n_games_per_agent, groupby_agent=True):
     if len(agents) != len(elos) or len(agents) != len(n_games_per_agent):
         raise ValueError("agent, elo, and n_games lists must be equal in length")
 
-    for i in range(len(agents)):
-        update_agent_elo(agents[i], elos[i], n_games_per_agent[i], commit=False)
+    # If the same agent is present more than once, average its updated elos
+    # Otherwise the default implementation stores the last given occurrence of the agent
+    if groupby_agent:
+        grouped_agents = {}
+        for i, agent in enumerate(agents):
+            if str(agent) not in grouped_agents:
+                grouped_agents[str(agent)] = [i]
+            else:
+                grouped_agents[str(agent)].append(i)
+
+        def average_list_over_indices(list_, indices):
+            return sum([list_[idx] for idx in indices])/len(indices)
+
+        for agent_name in grouped_agents:
+            update_agent_elo(agent_name,
+                             average_list_over_indices(elos, grouped_agents[agent_name]),
+                             average_list_over_indices(n_games_per_agent, grouped_agents[agent_name]),
+                             commit=False)
+
+    else:
+        # Default behavior, does not account for case when an agent is present more than once
+        for i in range(len(agents)):
+            update_agent_elo(agents[i], elos[i], n_games_per_agent[i], commit=False)
 
     db.session.commit()
 
